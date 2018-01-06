0e8453e4f363d383e8856cf97ed50e3666dc9de9
Lemmah <lemayiannakolah@gmail.com>
2018-01-06 23:54:38 +0300

    feat(answer_view): Add a Question Route to view it's answers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
   post '/question' => 'home#tmp'
+  get '/question/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
