9bee4d3a43382d85a82e96f416f1caa4a75ba04e
Lemmah <lemayiannakolah@gmail.com>
2018-01-09 00:00:28 +0300

    feat(answer_view): Create Garbage Route for Posting New Answers

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
   post '/questions' => 'home#tmp'
+  post '/answers' => 'home#tmp'
   get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
