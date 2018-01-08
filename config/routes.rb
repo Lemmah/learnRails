0477e77349def87e1441f126f6668b06d74ce764
Lemmah <lemayiannakolah@gmail.com>
2018-01-08 23:01:35 +0300

    feat(answer_view): Fix Route path typo
    question => questions
    
    It just makes sense to have endpoints plularized if they deal with a set of data.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'home#index'
   get '/about' => 'home#about'
-  post '/question' => 'home#tmp'
-  get '/question/:id' => 'home#question'
+  post '/questions' => 'home#tmp'
+  get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
