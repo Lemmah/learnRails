85b7381aac6ec95bc36b9df5de43d355fa87f3a9
Lemmah <lemayiannakolah@gmail.com>
2018-01-09 01:53:05 +0300

    feat(use_data): Create New Questions from UI
    Configure Controllers and Routes to handle the POST Request Correctly

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ Rails.application.routes.draw do
   resources :questions
   root 'home#index'
   get '/about' => 'home#about'
-  post '/questions' => 'home#tmp'
-  post '/answers' => 'home#tmp'
-  get '/questions/:id' => 'home#question'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
