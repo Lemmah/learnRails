273426dce4fd96b6d1164053c858e4c27e1f181a
Lemmah <lemayiannakolah@gmail.com>
2018-01-10 21:24:24 +0300

    feat(use_data): Explicitly Create Answer from existing Question

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -2,7 +2,7 @@ class AnswersController < ApplicationController
   
   def create
     question = Question.find(params[:answer][:question_id]) 
-    Answer.create(answer_params)
+    question.answers.create(answer_params)
     redirect_to question
   end
 
